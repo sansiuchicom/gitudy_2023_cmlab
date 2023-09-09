1b99f7c4efbd0054e65313421fad2730c549daa4
sansiuchicom <scot0320@snu.ac.kr>
2023-09-09 17:11:48 +0900

    20230909 the purpose of the gittudy is specified as a 'descrbe_gitudy_purpose.py' python file, for test purpose

---
+++ describe_gitudy_purpose.py
@@ -0,0 +1,9 @@
+
+def describe_gitudy_purpose():
+	print("""This is the record of the study doen by "Kim H., im, S, Shin, J., Baik J. and CPT.Lim M."
+		The purpose of the study is to learn how the git and github works.
+		Furthermore, We, as a CMLab members, hope to utilize this colloabrative systems as one of the research tools
+		""")
+
+if __name__=="main":
+	print(__name__)
