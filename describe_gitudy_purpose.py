6acb6c1281d0186b61ee5c2d7c87457108d168af
Kim, Seoktae <135973468+sansiuchicom@users.noreply.github.com>
2023-09-09 17:55:07 +0900

    0.1 Update describe_gitudy_purpose.py
    test

--- describe_gitudy_purpose.py
+++ describe_gitudy_purpose.py
@@ -1,10 +1,8 @@
-#Test
 def describe_gitudy_purpose():
 	print("""This is the record of the study done by "Kim H., Lim, S, Shin, J., Baik J. and CPT.Lim M."
-		The purpose of the study is to learn how the git and github works.
-		Furthermore, We, as a CMLab members, hope to utilize this colloabrative systems as one of the research tools
+The purpose of the study is to learn how the git and github works.
+Furthermore, We, as a CMLab members, hope to utilize this colloabrative systems as one of the research tools
 		""")
 
 
 describe_gitudy_purpose()
-	
